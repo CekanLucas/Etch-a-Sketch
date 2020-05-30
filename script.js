941d64a83927a289aa4c93c9aef3a6ffe9a9fa05
Lucas <cekanlucas@gmail.com>
2020-05-30 18:22:20 -0400

    dom munipulation, can create row divs

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 const btn = document.querySelector('#clickCreateElement');
-btn.onclick = e => console.log(e);
+$(document).ready( function() {
+  const container = document.getElementById('container')
+  
+  btn.onclick = e => console.log('row');
+  
+  btn.addEventListener('mouseleave', function (e) {
+    const row = document.createElement('div');
+    // row.setAttribute('id','continer');
+    row.classList.add('columns');
+    row.classList.add('row');
+    
+    const sqr = document.createElement('div');
+    sqr.classList.add('column');
+    sqr.classList.add('sqr');
 
-btn.addEventListener('mouseleave', function (e) {
-  e.target.style.background = 'red !important';
-});
+    row.appendChild(sqr)
+    container.appendChild(row)
+    console.log('test', container)
+    e.target.style.background = 'red !important';
+  });
+})
+
